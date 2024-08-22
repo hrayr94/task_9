8296e010d499da8e690f8129487fe1cb558efefe
unknown <hrayrashchyan94@gmail.com>
2024-08-22 18:34:58 +0400

    saveAuthors added

--- common/models/Book.php
+++ common/models/Book.php
@@ -1,19 +1,35 @@
 <?php
-
 namespace common\models;
 
 use Yii;
 use yii\db\ActiveRecord;
 
+/**
+ * This is the model class for table "books".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $description
+ * @property int|null $publication_year
+ *
+ * @property Author[] $authors
+ * @property BookAuthor[] $bookAuthors
+ */
 class Book extends ActiveRecord
 {
     public $author_ids = [];
 
+    /**
+     * {@inheritdoc}
+     */
     public static function tableName()
     {
         return 'books';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function rules()
     {
         return [
@@ -25,7 +41,9 @@ class Book extends ActiveRecord
         ];
     }
 
-
+    /**
+     * {@inheritdoc}
+     */
     public function attributeLabels()
     {
         return [
@@ -37,13 +55,42 @@ class Book extends ActiveRecord
         ];
     }
 
+    /**
+     * Gets query for [[Authors]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
     public function getAuthors()
     {
-        return $this->hasMany(Author::class, ['id' => 'author_id'])->via('bookAuthor', ['book_id' => 'id']);
+        return $this->hasMany(Author::class, ['id' => 'author_id'])->via('book_author', ['book_id' => 'id']);
     }
 
+    /**
+     * Gets query for [[BookAuthors]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
     public function getBookAuthors()
     {
         return $this->hasMany(BookAuthor::class, ['book_id' => 'id']);
     }
+
+    /**
+     * Save selected authors
+     */
+
+    public function saveAuthors()
+    {
+        \common\models\BookAuthor::deleteAll(['book_id' => $this->id]);
+
+        if (is_array($this->author_ids)) {
+            foreach ($this->author_ids as $author_id) {
+                $bookAuthor = new \common\models\BookAuthor();
+                $bookAuthor->book_id = $this->id;
+                $bookAuthor->author_id = $author_id;
+                $bookAuthor->save();
+            }
+        }
+    }
+
 }
